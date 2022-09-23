5ece3a8670941f44a73f50dff75bbc61bacd27a0
liquancool <llei0111@bths.edu>
2022-09-23 17:56:52 -0400

    added comments

--- src/U2L2Runner.java
+++ src/U2L2Runner.java
@@ -8,9 +8,9 @@ public class U2L2Runner {
         squareRec.setLength(125);
         squareRec.setWidth(125);
         defRec.setWidth(125);
-        int customRecPerimeter = customRec.calculatePerimeter();
-        int squareRecPerimeter = squareRec.calculatePerimeter();
-        int defRecPerimeter = defRec.calculatePerimeter();
+        int customRecPerimeter = (customRec.getLength() + customRec.getWidth()) * 2;//this calculates the perimeter
+        int squareRecPerimeter = (squareRec.getLength() + squareRec.getWidth()) * 2;
+        int defRecPerimeter = (defRec.getLength() + defRec.getWidth()) * 2;
         int recPerimeterSum = customRecPerimeter + squareRecPerimeter + defRecPerimeter;
         int customRecArea = customRec.calculateArea();
         int squareRecArea = squareRec.calculateArea();
@@ -26,9 +26,9 @@ public class U2L2Runner {
         int windows = s.nextInt();
         System.out.println("What is the tax rate for your area?");
         double tax = s.nextDouble();
-        ConstructionPricer price1 = new ConstructionPricer(11.50, 25.75, tax);
-        double totalNoTax = price1.materialsCost(lumberPieces, windows);
-        double grandTotal = price1.totalWithTax(totalNoTax);
+        ConstructionPricer price1 = new ConstructionPricer(11.50, 25.75, tax);//fills the parameters of the constructor
+        double totalNoTax = price1.materialsCost(lumberPieces, windows);//this method calculates the cost of materials only
+        double grandTotal = price1.totalWithTax(totalNoTax);//this method calculates the grand total including tax
         System.out.println("Total Materials Cost: " + totalNoTax);
         System.out.println("Grand Total after Tax: " + grandTotal);
     }
